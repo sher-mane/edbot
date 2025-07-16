e61f6fc40a88ff165f75dacf518cf8a7186c44b6
shermane <edsherman88@gmail.com>
2025-07-16 16:36:32 -0500

    setup fixes for pip and apt

--- pi_setup.sh
+++ pi_setup.sh
@@ -4,4 +4,5 @@ sudo apt install imx500-all
 sudo apt install rpi-connect
 sudo apt install portaudio19-dev
 sudo apt install espeak ffmpeg portaudio19-dev flac
+sudo apt install mpg123
 
--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-sudo apt install portaudio19-dev
+
 pip install pyaudio
 pip install openai
 pip install pyttsx3
+pip install SpeechRecognition
 
+
+START WITH ABOVE AND ONLY USE BELOW IF NEEDED
 # for whisper via OPENAI Cloud
 #pip install requests
 
+
+
+
 #
-pip install git+https://github.com/openai/whisper.git
-pip install torch  # Requires compatible PyTorch version for your Pi (may need manual install)
+#pip install git+https://github.com/openai/whisper.git
+#pip install torch  # Requires compatible PyTorch version for your Pi (may need manual install)
